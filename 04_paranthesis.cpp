d4388dfb90e5eab60a074c95c4f56c16e57338b3
KAUSHAL KIRAN BANGALORE SAMPATH KUMAR <kkbangal@uwaterloo.ca>
2022-01-06 00:06:38 -0500

    stack working

---
+++ 04_paranthesis.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int
+main() {
+
+	string s = "((()))";
+	string s_new = "{{{}}}";
+
+	for (char &c : s) {
+		cout << c << endl;
+		int i = c;
+		cout << i << endl;
+	}
+
+	stack<char> test;
+	stack<char> test2;
+
+	for (char &t : s) {
+		test.emplace(t);
+		//test2.push(t);
+	}
+
+	for (char &t : s_new) {
+		test2.push(t);
+	}
+
+	test.swap(test2);
+
+	// Size of the stack
+	
+	cout << "The size of the stack is : " << test.size();
+
+	// using emplace 	
+
+	cout << "Using emplace to populate the stack " << endl;
+
+	while(!test.empty()) {
+		cout << test.top() << endl;
+		test.pop();
+	}
+
+	// using push
+	cout << "Using push to populate the stack " << endl;
+
+	while(!test2.empty()) {
+		cout << test2.top() << endl;
+		test2.pop();
+	}
+
+	return EXIT_SUCCESS;
+}
